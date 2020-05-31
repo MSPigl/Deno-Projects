21396a34328d06b1e95a500fb43e62152ccca9e0
Matt <mattep96@gmail.com>
2020-05-31 13:29:08 -0400

    Delegate querystring building to function, add getUpcomingCapsules func

--- spacex-api/api.ts
+++ spacex-api/api.ts
@@ -1,9 +1,24 @@
 import api from 'https://deno.land/x/api/index.ts';
-import { Capsule, CapsuleParams } from './models';
+import { Capsule, CapsuleParams } from './models/index.ts';
 
 const BASE_PATH = 'https://api.spacexdata.com/v3';
 
 export async function getAllCapsules(queryParams?: CapsuleParams): Promise<Array<Capsule>> {
+    const querystring = buildQueryString(queryParams);
+    return api.get(`${BASE_PATH}/capsules${querystring}`);
+}
+
+export async function getCapsuleBySerial(serial: string, id = false): Promise<Capsule> {
+    const response = await api.get(`${BASE_PATH}/capsules/${serial}${id ? '?id=true' : ''}`);
+    return !!response['error'] ? Promise.reject(response['error']) : response;
+}
+
+export async function getUpcomingCapsules(queryParams?: CapsuleParams): Promise<Array<Capsule>> {
+    const querystring = buildQueryString(queryParams);
+    return api.get(`${BASE_PATH}/capsules/upcoming${querystring}`);
+}
+
+function buildQueryString(queryParams: any): string {
     let querystring = '';
 
     if (queryParams && Object.keys(queryParams).length) {
@@ -15,10 +30,5 @@ export async function getAllCapsules(queryParams?: CapsuleParams): Promise<Array
         querystring = querystring.substring(0, querystring.length - 1);
     }
 
-    return api.get(`${BASE_PATH}/capsules${querystring}`);
-}
-
-export async function getCapsuleBySerial(serial: string, id = false): Promise<Capsule> {
-    const response = await api.get(`${BASE_PATH}/capsules/${serial}${id ? '?id=true' : ''}`);
-    return !!response['error'] ? Promise.reject(response['error']) : response;
+    return querystring;
 }

673b9e27aa1f62f43bd939708ecc663448db6729
Matt <mattep96@gmail.com>
2020-05-31 13:18:56 -0400

    Set up module system for models, refactor capsule params into a model

--- spacex-api/api.ts
+++ spacex-api/api.ts
@@ -1,25 +1,9 @@
 import api from 'https://deno.land/x/api/index.ts';
-import { Capsule } from './models/capsule.model.ts';
+import { Capsule, CapsuleParams } from './models';
 
 const BASE_PATH = 'https://api.spacexdata.com/v3';
 
-export async function getAllCapsules(
-    queryParams?: {
-        capsule_serial?: string, // query by
-        capsule_id?: string,
-        status?: string,
-        original_launch?: string,
-        mission?: string,
-        landings?: number,
-        type?: string,
-        reuse_count?: number,
-        id?: boolean, // output control
-        limit?: number,
-        offset?: number,
-        sort?: 'capsule_serial' | 'capsule_id' | 'status' | 'original_launch' | 'mission' | 'landings' | 'type' | 'reuse_count',
-        order?: 'asc' | 'desc'
-    }
-): Promise<Array<Capsule>> {
+export async function getAllCapsules(queryParams?: CapsuleParams): Promise<Array<Capsule>> {
     let querystring = '';
 
     if (queryParams && Object.keys(queryParams).length) {
